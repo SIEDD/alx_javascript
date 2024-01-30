1fb2e2541756fa881acb20c96760b3dab5d9f09d
siedd <saidmwacharo8@gmail.com>
2024-01-30 23:58:27 +0300

    repeat

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-    if (w > 0 && h > 0) {
-        this.width = w=3;
+    if (w>0 && h>0) {
+        this.width = w=2;
         this.height = h=3;
         } 
     }
     print () {
-        const chr = 'X'
+        const chr = 'X';
         for (let i = 0; i < this.height; i++){
             console.log(chr.repeat(this.width))
         }
