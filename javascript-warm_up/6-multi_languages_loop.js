8cc7a86039370464d582ab1086910b6e6657cee4
siedd <saidmwacharo8@gmail.com>
2024-01-30 22:51:20 +0300

    loops

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,8 @@
+// Array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and print each line
+for (let i = 0; i < languages.length; i++) {
+    console.log(languages[i]);
+}
 
