89b90a708f89d5f8573238b1e00dcf321e77e422
siedd <saidmwacharo8@gmail.com>
2024-01-31 00:22:40 +0300

    rotate

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,30 @@
-
+class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+        this.width = w;
+        this.height = h;
+      } 
+    }
+  
+    print() {
+      // Check if the instance is not empty
+      if (this.width && this.height) {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      } 
+    }
+  
+    rotate() {
+      // Swap the values of width and height
+      [this.width, this.height] = [this.height, this.width];
+    }
+  
+    double() {
+      // Double the values of width and height
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+  module.exports = Rectangle;
+  
