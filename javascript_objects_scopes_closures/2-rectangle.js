e77109b3acaac623d734dc79978873d93bd75151
siedd <saidmwacharo8@gmail.com>
2024-01-30 23:35:34 +0300

    if

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-      if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
-      } else {
-        console.warn("Invalid width or height. Creating an empty rectangle.");
-      }
+      } 
     }
 }
 module.exports = Rectangle; 
