559256459a7b51377b6eb8ff3078485b9e267ac5
siedd <saidmwacharo8@gmail.com>
2024-01-30 22:52:43 +0300

    replace

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,1 +1,13 @@
+// Array of numbers
+const numbers = [3, 12, 7, 4, 89, 23, 17];
+
+// Loop through the array and replace the value 12 with 89
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 12) {
+        numbers[i] = 89;
+    }
+}
+
+// Print the updated array
+console.log(numbers);
 
