5b2fb6b1856cd5fc80ea0ab543e174b20c4dc115
siedd <saidmwacharo8@gmail.com>
2024-01-31 10:12:42 +0300

    export

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -2,8 +2,22 @@ const Rectangle = require('./4-rectangle.js');
 
 class Square extends Rectangle {
   constructor(size) {
+    
     super(size, size);
   }
 }
 module.exports = Rectangle;
+
+const mySquare = new Square(5);
+console.log(mySquare); 
+mySquare.print();
+
+
+mySquare.rotate();
+console.log("Square after rotate():");
+mySquare.print();
+
+mySquare.double();
+console.log("Square after double():");
+mySquare.print();
 
