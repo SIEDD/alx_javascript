d15f09b2bbd03d4cb90f4689359ec6f26eec030d
siedd <saidmwacharo8@gmail.com>
2024-02-08 23:23:51 +0300

    starwars

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,1 +1,14 @@
+const process = require('process');
+const request = require('request');
+
+const movieID = process.argv[2];
+const requestURL = `https://swapi-api.alx-tools.com/api/films/${movieID}`
+request(requestURL, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        const movieData = JSON.parse(body);
+        console.log(movieData.title);
+    } else {
+        console.error("Error:", error);
+    }
+});
 
