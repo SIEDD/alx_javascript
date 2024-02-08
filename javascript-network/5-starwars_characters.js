464850313b689d94e0ce73f650c01cad1cbed787
siedd <saidmwacharo8@gmail.com>
2024-02-08 23:49:54 +0300

    movies

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,1 +1,23 @@
+const request = require('request');
+const Id = process.argv[2];
+const url = `https://swapi.dev/api/films/${Id}/`;
 
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  for (const character of characters) {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  }
+});
