76aef4cef47399997e3bc7ccfa1c0e71989010fb
siedd <saidmwacharo8@gmail.com>
2024-02-08 23:13:39 +0300

    statuscode

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,1 +1,11 @@
+// script that display the status code.
 
+const process = require('process')
+const request = require('request')
+const requestURL = process.argv[2]
+
+request(requestURL, function(error, response){
+    if(error == null){
+        console.log('code: ' + response.statusCode);
+    };
+});
