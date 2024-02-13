80b699d241ff72447c61dfd956421cf52d4f6168
siedd <saidmwacharo8@gmail.com>
2024-02-13 14:53:01 +0300

    javascript

--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -1,1 +1,12 @@
-
+function welcomeMessage(fullName) {
+    // Define a closure for alerting the welcome message
+    return function() {
+      alert('Welcome ' + fullName);
+    };
+  }
+  
+  // Create variables using the welcomeMessage function
+  var guillaume = welcomeMessage('Guillaume');
+  var alex = welcomeMessage('Alex');
+  var fred = welcomeMessage('Fred');
+  
