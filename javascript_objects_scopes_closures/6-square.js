57e4502d054b546ec87b631321d3cc06034c425f
siedd <saidmwacharo8@gmail.com>
2024-02-13 13:37:54 +0300

    square

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,11 @@
-
+// Square that inherits form the prevoius file
+const Square1 = require('./5-square')
+class Square extends Square1{
+    charPrint(c){
+        const char = c || 'X';
+        for (let i = 0; i < this.height; i++) {
+            console.log(char.repeat(this.width));
+        };
+    };
+};
+module.exports = Square;
